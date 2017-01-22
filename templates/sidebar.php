8d9c59c1ebdeb2714bd5ee1d3c6524cd7f8c6d56
Pete Bernardo <peterbernardo@gmail.com>
2017-01-22 16:04:44 -0500

    sidebar and icons

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -17,7 +17,12 @@
 				</aside>
 				<aside id="categories">
 				<div class="row">
-					<div class="col-sm-4"><a href="<?php $category_id = get_cat_ID( 'Photos' );$category_link = get_category_link( $category_id );echo($category_link);$thisCat = get_category($category_id);?> "><span class="bold"><?php echo $thisCat->count;?></span><span class="catName">Photos</span></a></div>
+					<div class="col-sm-4">
+						<a href="<?php $category_id = get_cat_ID( 'Photos' );$category_link = get_category_link( $category_id );echo($category_link);$thisCat = get_category($category_id);?> ">
+							<span class="bold"><?php echo $thisCat->count;?></span>
+							<span class="catName">Photos</span>
+						</a>
+						</div>
 					<div class="col-sm-4 borderRight"><a href="<?php $category_id = get_cat_ID( 'Articles' );$category_link = get_category_link( $category_id );echo($category_link);$thisCat = get_category($category_id);?> "><span class="bold"><?php echo $thisCat->count;?></span><span class="catName">Articles</span></a></div>
 					<div class="col-sm-4"><a href="<?php $category_id = get_cat_ID( 'Inspirations' );$category_link = get_category_link( $category_id );echo($category_link);$thisCat = get_category($category_id);?> "><span class="bold"><?php echo $thisCat->count;?></span><span class="catName">Inspirations</span></a></div>
 				</div>
@@ -27,10 +32,10 @@
 				</aside>
 				<aside>
 					<ul class="where-else">
-						<li class="twitter"><a href="http://twitter.com/petebernardo" rel="tooltip" title="twitter.com/petebernardo"><i class="fa fa-twitter"></i></a></li>
-						<li class="facebook"><a href="http://facebook.com/petebernardo" rel="tooltip" title="facebook.com/petebernardo"><i class="fa fa-facebook"></i></a></li>
-						<li class="linkedin"><a href="http://www.linkedin.com/in/petebernardo/" rel="tooltip" title="linkedin.com/in/petebernardo/"><i class="fa fa-linkedin"></i></a></li>
-						<li class="instagram"><a href="http://www.instagram.com/petebernardo/" rel="tooltip" title="instagram.com/petebernardo"><i class="fa fa-camera-retro"></i></a></li>
+						<li><a href="http://twitter.com/petebernardo" rel="tooltip" title="twitter.com/petebernardo"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="http://facebook.com/petebernardo" rel="tooltip" title="facebook.com/petebernardo"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="http://www.linkedin.com/in/petebernardo/" rel="tooltip" title="linkedin.com/in/petebernardo/"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="http://www.instagram.com/petebernardo/" rel="tooltip" title="instagram.com/petebernardo"><i class="fa fa-camera-retro"></i></a></li>
 					</ul>
 				</aside>
 				<aside class="quote"><a href="https://twitter.com/jack/status/17680161621151744" rel="tooltip" data-toggle="tooltip" title="What I believe in.">Make every detail perfect and limit the number of details to perfect.<br/></a><span class="small">- @jack</span></aside>
